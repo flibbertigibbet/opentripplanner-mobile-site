892125f890bfded9fbdc34e2667d6a1758256a3d
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-06-10 23:13:31 -0400

    Add grunt server

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,25 @@ module.exports = function(grunt) {
         options: {
         }
       }
+    },
+    'http-server': {
+        cache: 5,
+        port: 8282,
+        host: "127.0.0.1",
+        // server default file extension 
+        ext: "html",
+        root: '.'
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-http-server');
 
   // Default task(s).
   grunt.registerTask('default', ['wiredep']);
+
+  // basic web server for testing
+  grunt.registerTask('serve', ['http-server']);
+  
 };
