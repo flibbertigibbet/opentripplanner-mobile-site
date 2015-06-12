f112b745dda40a588931b117dc9abe7e34ba35ae
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-06-11 22:07:35 -0400

    Add deployment tasks and grunt task to push to gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,127 @@
 module.exports = function(grunt) {
+    require('time-grunt')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    wiredep: {
-      task: {
-        src: [
-          'index.html',
-          'styles/main.css'
-        ],
-        exclude: [ /leaflet/ ],
-        options: {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['dist'],
+        copy: {
+            app: {
+                files: [
+                    { src: 'index.html', dest: 'dist/' },
+                    { expand: true, src: 'img/**', dest: 'dist' },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/**/*.{png,jpg,ico,svg}',
+                        dest: 'dist/styles/images'
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            bower: {
+                files: [{
+                    dest: 'dist/styles/vendor.css',
+                    src: require('wiredep')({exclude: [ /leaflet/ ]}).css
+                }]
+            },
+            app: {
+                files: [{ dest: 'dist/styles/app.css', src: 'styles/**/*.css' }]
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                screwIE8: true,
+                sourceMap: true
+            },
+            bower: {
+                files: [{
+                    dest: 'dist/scripts/vendor.js',
+                    src: require('wiredep')({exclude: [ /leaflet/ ]}).js
+                }]
+            },
+            app: {
+                files: [{ dest: 'dist/scripts/app.js', src: 'js/**/*.js' }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            },
+        },
+        wiredep: {
+            task: {
+                src: [
+                  'index.html',
+                  'styles/main.css'
+                ],
+                exclude: [ /leaflet/ ]
+            }
+        },
+        'http-server': {
+            cache: 1,
+            port: 8282,
+            host: "127.0.0.1",
+            // server default file extension 
+            ext: "html",
+            root: '.'
+        },
+        'gh-pages': {
+            options: {
+                base: 'dist'
+            },
+            src: ['**']
+        },
+        useminPrepare: {
+            html: 'dist/index.html',
+        },
+        usemin: {
+            dest: 'dist',
+            html: 'dist/index.html',
+            css: ['vendor.css', 'app.css'],
+            js: ['vendor.js', 'app.js']
         }
-      }
-    },
-    'http-server': {
-        cache: 5,
-        port: 8282,
-        host: "127.0.0.1",
-        // server default file extension 
-        ext: "html",
-        root: '.'
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-wiredep');
-  grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-filerev');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-wiredep');
 
-  // Default task(s).
-  grunt.registerTask('default', ['wiredep']);
+    // basic web server for testing
+    grunt.registerTask('serve', ['http-server']);
 
-  // basic web server for testing
-  grunt.registerTask('serve', ['http-server']);
-  
+    // Default task(s).
+    grunt.registerTask('default', ['wiredep', 'serve']);
+
+    // build for production
+    grunt.registerTask('build', [
+        'clean',
+        'wiredep',
+        'copy:app',
+        'useminPrepare',
+        'cssmin:bower',
+        'cssmin:app',
+        'uglify:bower',
+        'uglify:app',
+        'usemin',
+        'htmlmin:dist'
+    ]);
+
+    // push to gh-pages
+    grunt.registerTask('deploy', ['build', 'gh-pages'])
 };
